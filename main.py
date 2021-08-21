8cb1bf3cf550a83a77276cb26d23d3e13f7cc7b1
Darius Weber <darius.weber@posteo.de>
2021-08-21 23:49:59 +0200

    Add some exception handling for gracefull shutdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import sys
+
 import structlog
 import ecs_logging
 
@@ -8,45 +10,28 @@ from time import sleep
 from collecting.kasa import KasaCollector
 from models.configuration import Configuration
 
+# configure logger
 structlog.configure(
     processors=[ecs_logging.StructlogFormatter()],
     wrapper_class=structlog.BoundLogger,
     context_class=dict,
     logger_factory=structlog.PrintLoggerFactory(),
 )
+__logger = structlog.get_logger("kasa-reporter")
 
+# configure argument parser
 parser = ArgumentParser()
 
-# device ip or hostname
-parser.add_argument(
-    '-d', '--device',
-    type=str,
-    required=True,
-)
-
-# device type
-parser.add_argument(
-    '-t', '--type',
-    type=str,
-    choices=['plug'],
-    default='plug'
-)
-
-# fetch interval in seconds
-parser.add_argument(
-    '-i', '--interval',
-    type=int,
-    default=5,
-    required=False
-)
+parser.add_argument('-d', '--device', type=str, required=True)  # device ip or hostname
+parser.add_argument('-t', '--type', type=str, choices=['plug'], default='plug')  # device type
+parser.add_argument('-i', '--interval', type=int, default=5, required=False)  # fetch interval in seconds
 
 
 async def main():
     args = parser.parse_args()
     configuration = Configuration(args)
 
-    logger = structlog.get_logger("kasa-reporter")
-    logger = logger.bind(**{
+    logger = __logger.bind(**{
         'configuration': {
             'device': configuration.device,
             'device_type': configuration.device_type,
@@ -54,7 +39,6 @@ async def main():
         },
     })
 
-    # print(f'Start fetching emeter data for {configuration.device} as {configuration.device_type} in {configuration.interval}s interval.')
     logger.info('Start fetching data.')
     collector = KasaCollector(configuration)
     await collector.setup()
@@ -66,4 +50,11 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        __logger.info('Application shutdown requested by interruption.')
+        sys.exit(0)
+    except Exception as e:
+        __logger.fatal('Something terrible happened!', exception=e)
+        sys.exit(1)
