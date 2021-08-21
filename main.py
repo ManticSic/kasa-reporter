52f6c61814aa4d3f3384d63673af8b4801147ca8
Darius Weber <darius.weber@posteo.de>
2021-08-21 23:05:19 +0200

    Add structured logging
    - add structlog for logging
    - use ecs format for structured logging
    - log application start
    - log fetched data

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import asyncio
+import structlog
+import ecs_logging
 
 from argparse import ArgumentParser
 from time import sleep
 
 from collecting.kasa import KasaCollector
 from models.configuration import Configuration
+
+structlog.configure(
+    processors=[ecs_logging.StructlogFormatter()],
+    wrapper_class=structlog.BoundLogger,
+    context_class=dict,
+    logger_factory=structlog.PrintLoggerFactory(),
+)
 
 parser = ArgumentParser()
 
@@ -36,15 +45,23 @@ async def main():
     args = parser.parse_args()
     configuration = Configuration(args)
 
-    print(f'{repr(configuration)}')
+    logger = structlog.get_logger("kasa-reporter")
+    logger = logger.bind(**{
+        'configuration': {
+            'device': configuration.device,
+            'device_type': configuration.device_type,
+            'interval': configuration.interval,
+        },
+    })
 
-    print(f'Start fetching emeter data for {configuration.device} as {configuration.device_type} in {configuration.interval}s interval.')
+    # print(f'Start fetching emeter data for {configuration.device} as {configuration.device_type} in {configuration.interval}s interval.')
+    logger.info('Start fetching data.')
     collector = KasaCollector(configuration)
     await collector.setup()
 
     while True:
         data = await collector.fetch()
-        print(f'{data}')
+        logger.info('Successfully fetched data.', data=data)
         sleep(configuration.interval)
 
 
