5f628ebdbaf325b648ac2bba332af8709cbf9451
Darius Weber <darius.weber@posteo.de>
2021-08-27 20:54:06 +0200

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./main.py -d$DEVICE -i$INTERVAL --logstash-host $LOGSTASH_HOST --logstash-port $LOGSTASH_PORT
+python3 ./main.py --fetch-interval $FETCH_INTERVAL --discover-interval $DISCOVER_INTERVAL --logstash-host $LOGSTASH_HOST --logstash-port $LOGSTASH_PORT
